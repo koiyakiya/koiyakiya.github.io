732652b3bc5faad84b4ff41a4a5b77369ca06c51
koiyakiya <koiyakiya9@gmail.com>
2024-07-22 03:14:49 -0500

    test commit

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import './tailwind.postcss'
 import DefaultTheme from 'vitepress/theme'
-import VueKofi from 'vue-kofi';
+const VueKofi = require('vue-kofi')
 import './style.css'
 import './custom.css'
 import MyLayout from './MyLayout.vue'
